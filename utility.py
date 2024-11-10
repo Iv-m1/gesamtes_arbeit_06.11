bbe90c2e46bd97c3c76855727690323202a0f74d
EugenM <maljaseugen@gmail.com>
2024-11-10 19:38:37 +0100

    Games Hangman 1.02

---
+++ utility.py
@@ -0,0 +1,8 @@
+from guess_number import play_guess_number
+from hangman import play_hangman
+from minesweeper import play_minesweeper
+from quiz_game import play_quiz_game
+from rock_paper_scissors import play_rock_paper_scissors
+from text_adventure import play_text_adventure
+
+# FINAL
